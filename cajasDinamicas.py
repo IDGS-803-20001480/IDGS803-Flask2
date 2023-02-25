fe3e4e29c85ae612c3181af2edc210b877d88577
IDGS-803-20001480 <jonathanutlpruebas@gmail.com>
2023-02-24 20:36:32 -0600

    clase 18/02/23

--- cajasDinamicas.py
+++ cajasDinamicas.py
@@ -16,9 +16,41 @@ def cajas():
 
 @app.route("/resultado", methods=['POST'])
 def resultado():
+    numerosProcesados = []
     numeros = request.form
-    print(numeros)
-    return render_template("resultado.html", numeros=int(numeros))
+    print(len(numeros))
+    i = 1
+    while i <= len(numeros):
+        numerosProcesados.append(numeros.get('txtCaja'+str(i)))
+        i += 1
+    print(numerosProcesados)
+
+    #sacar el numero menor y mayor de la lista numeros procesados
+    mayor = 0
+    menor = int(numerosProcesados[0])
+    suma = 0
+    for i in numerosProcesados:
+        suma += int(i)
+        if int(i) > mayor:
+            mayor = int(i)
+        if int(i) < menor:
+            menor = int(i)
+
+    print(mayor)
+    print(menor)
+    promedio = suma / len(numerosProcesados)
+    print(promedio)
+
+    #saber si un numero se repite mas de 1 vez
+    repetidos = []
+    for i in numerosProcesados:
+        if numerosProcesados.count(i) > 1:
+            texto = "El numero " + i + " se repite " + str(numerosProcesados.count(i)) + " veces"
+            if texto not in repetidos:
+                repetidos.append(texto)
+    print(repetidos)
+
+    return render_template("resultado.html",suma=suma, mayor=mayor, menor=menor, promedio=promedio, repetidos=repetidos,numerosIngresados=len(numerosProcesados))
 
 
 
