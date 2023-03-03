3f760f061a4d2914bffeb4f31e4aef16a4adbb6e
IDGS-803-20001480 <jonathanutlpruebas@gmail.com>
2023-03-03 17:44:58 -0600

    clase de 25/02/23

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app=Flask(__name__)
 app.config['SECRET_KEY']= "Esta es la clave encriptadora"
 csrf = CSRFProtect()
 
+#-----------------------Cokiees--------------------------------
 @app.route("/cookies",methods=['GET', 'POST'])
 def cookies():
     reg_user=forms.LoginForm(request.form)
@@ -49,6 +50,108 @@ def alumnos():
         print(alum_form.matricula.data)
         print(alum_form.nombre.data)
     return render_template("Alumnos.html",form=alum_form)
+
+
+
+#-----------------------Traductor archivo de texto--------------------------------
+
+
+@app.route("/traductor", methods=['GET', 'POST'])
+def traductor():
+    respuesta=""
+    trad_form = forms.TraductorForm(request.form)
+    trad_form_response = forms.TraductorFormRespose(request.form)
+    if request.method == 'POST' and trad_form.validate():
+        f=open("Traductor.txt", "a")
+        f.write("\n"+ ""+trad_form.palabra.data+"="+trad_form.traduccion.data+"")
+        f.close()
+        respuesta = trad_form.palabra.data
+        print(trad_form.palabra.data)
+        print(trad_form.traduccion.data)
+    return render_template("traductor.html",respAdd=respuesta,form=trad_form,form2=trad_form_response)
+
+@app.route("/traductor_response", methods=['GET', 'POST'])
+def traductor_response():
+    respuesta=""
+    trad_form = forms.TraductorForm(request.form)
+    trad_form_response = forms.TraductorFormRespose(request.form)
+    if request.method == 'POST' and trad_form_response.validate():
+        palabra = trad_form_response.palabra_Traducir.data
+        opc = trad_form_response.radioIdiomaTraducir.data
+        print(palabra+" idioma:"+opc)
+        if opc=="es":
+            f=open("Traductor.txt", "r")
+            listaTraductor = f.readlines()
+            for linea in listaTraductor:
+              palabraIndicador=str(linea.split("=")[0])
+              if palabraIndicador == palabra:
+                  print(linea)
+                  print(linea.split("=")[1])
+                  respuesta=linea.split("=")[1]
+            f.close()
+            if respuesta == "":
+                respuesta = "No se encontro la palabra"
+            print(respuesta)
+            
+        elif opc=="en":
+            print("Ingles")
+            f=open("Traductor.txt", "r")
+            listaTraductor = f.readlines()
+            for linea in listaTraductor:
+                palabraIndicador=str(linea.split("=")[1])
+                palabraIndicador= palabraIndicador.strip()
+                print(palabraIndicador+"n")
+                if palabraIndicador == palabra:
+                    print(linea)
+                    print(linea.split("=")[0])
+                    respuesta=linea.split("=")[0]
+            f.close()
+            if respuesta == "":
+                respuesta = "No se encontro la palabra"
+            print(respuesta)
+    return render_template("traductor.html",resp=respuesta,entrada = palabra,form2=trad_form_response,form=trad_form)
+
+#-----------------------Calculadora de resistencias--------------------------------
+@app.route("/resistencias", methods=['GET', 'POST'])
+def resistencias():
+    colores = [("0","black"),("1","brown"),("2","red"),("3","orange"),("4","yellow"),("5","green"),("6","blue"),("7","violet"),("8","grey"),("9","white")]
+    coloresMultiplicador = [("1","black"),("10","brown"),("100","red"),("1000","orange"),("10000","yellow"),("100000","green"),("1000000","blue"),("10000000","violet"),("100000000","grey"),("1000000000","white"),("0.1","gold"),("0.01","silver")]
+    coloresTolerancia = [("1","brown"),("2","red"),("0.5","green"),("0.25","blue"),("0.1","violet"),("0.05","grey"),("5","gold"),("10","silver")]
+    color1 = ""
+    color2 = ""
+    colorMulti = ""
+    colorTol = ""
+    max = 0
+    min = 0
+    result = 0
+    resistencia_form = forms.ResistenciaForm(request.form)
+    if request.method == 'POST' and resistencia_form.validate():
+        
+        for color in colores:
+            if color[0] == resistencia_form.linea1.data:
+                color1 =  color[1]
+            if color[0] == resistencia_form.linea2.data:
+                color2 = color[1]
+        for color in coloresMultiplicador:
+            if color[0] == resistencia_form.lineaMultiplicador.data:
+                colorMulti = color[1]
+        for color in coloresTolerancia:
+            if color[0] == resistencia_form.lineaTolerancia.data:
+                colorTol = color[1]
+
+        
+        result = ((int(resistencia_form.linea1.data)*10) + int(resistencia_form.linea2.data))*float(resistencia_form.lineaMultiplicador.data)
+        min = result * (1-(float(resistencia_form.lineaTolerancia.data)/100))
+        max = result * (1+(float(resistencia_form.lineaTolerancia.data)/100))
+        print("Resultado: "+str(result))
+        print("Minimo: "+str(min))
+        print("Maximo: "+str(max))
+        print("Color 1: "+color1)
+        print("Color 2: "+color2)
+        print("Color Multiplicador: "+colorMulti)
+        print("Color Tolerancia: "+colorTol)
+    return render_template("calculadoraResistencias.html",form=resistencia_form,banda1=color1,banda2=color2,bandaMulti=colorMulti,bandaTol=colorTol,resultado=result,minimo=min,maximo=max)
+
 
 if __name__=="__main__":
     csrf.init_app(app)
--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 
 from wtforms import Form
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, FormField, RadioField
+from wtforms import StringField, SubmitField, FieldList, FormField, RadioField, SelectField
 from wtforms.fields import EmailField
 from wtforms import validators
 
@@ -23,4 +23,24 @@ class LoginForm(Form):
         validators.DataRequired(message='El campo es requerido'),validators.Length(min=4, max=15, message='No cumple la longitud para el campo')])
     password = StringField('password',[
         validators.DataRequired(message='El campo es requerido'),validators.Length(min=4, max=15, message='No cumple la longitud para el campo')])
-    
+
+class TraductorForm(Form):
+    palabra = StringField('Palabra en ingles ', [
+        validators.DataRequired(message='El campo es requerido'),validators.Length(min=4, max=15, message='No cumple la longitud para el campo')])
+    traduccion = StringField('Traduccion en español',[
+        validators.DataRequired(message='El campo es requerido'),validators.Length(min=4, max=15, message='No cumple la longitud para el campo')])
+
+class TraductorFormRespose(Form):
+    palabra_Traducir = StringField('Introduce la palabra que quieres traducir', [
+        validators.DataRequired(message='El campo es requerido'),validators.Length(min=3, max=15, message='No cumple la longitud para el campo')])
+    radioIdiomaTraducir = RadioField('Idioma a traducir',choices=[('es', 'Español'), ('en', 'Ingles')],validators=[validators.DataRequired()],default='es')
+
+class ResistenciaForm(Form):
+    #linea 1 de la resistencia de 4 lineas
+    colores = [("0","Negro"),("1","Marron"),("2","Rojo"),("3","Naranja"),("4","Amarillo"),("5","Verde"),("6","Azul"),("7","Violeta"),("8","Gris"),("9","Blanco")]
+    coloresMultiplicador = [("1","Negro"),("10","Marron"),("100","Rojo"),("1000","Naranja"),("10000","Amarillo"),("100000","Verde"),("1000000","Azul"),("10000000","Violeta"),("0.1","Dorado"),("0.01","Plateado")]
+    coloresTolerancia = [("1","Marron"),("2","Rojo"),("0.5","Verde"),("0.25","Azul"),("0.1","Violeta"),("0.05","Gris"),("5","Dorado"),("10","Plateado")]
+    linea1 = SelectField("linea 1: ",choices=colores)
+    linea2 = SelectField("linea 2: ",choices=colores)
+    lineaMultiplicador = SelectField("linea multiplicador: ",choices=coloresMultiplicador)
+    lineaTolerancia = SelectField("linea tolerancia: ",choices=coloresTolerancia)
